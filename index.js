ad2969dd2413187a5a03b4c4824ed84d7d2409f4
delubio de paula <delubiod@gmail.com>
2018-09-01 16:53:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+  let String = new RegExp(/^\s+$|^$/gi);
+  String.slice(-1);
+}
+
+// Defines a Phrase object. CONTENT PROPERTY
+function Phrase(content) {
+  this.content = content;
+
+
+  // Returns content processed for palindrome testing.
+    this.processedContent = function processedContent() {
+      // return this.content.toLowerCase();
+      return this.content.toLowerCase();
+    }
+
+    // Returns true if the phrase is a palindrome, false otherwise.
+    this.palindrome = function palindrome() {
+      return this.processedContent() === this.processedContent().reverse();
+    }
+  }
